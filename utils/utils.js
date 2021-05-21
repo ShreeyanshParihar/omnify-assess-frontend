e1e65bd80c0e5d67f0ea15c216e0c2e6d7f55c95
shree <shreeyanshparihar@gmail.com>
2021-05-22 03:04:37 +0530

    new push

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
 export class Values{
-   static server = "https://omnify-assess-server.herokuapp.com/api/";
+    static server = "https://omnify-assess-server.herokuapp.com/api/";
    //static server = "http://127.0.0.1:8000/api/";
 }
